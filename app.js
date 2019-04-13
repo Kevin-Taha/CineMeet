2836b3176aab92b56e0758545903689dc556e529
kevintxy <ktaha@purdue.edu>
2019-04-13 14:15:54 -0400

    NPM Init

---
+++ app.js
@@ -0,0 +1,13 @@
+// in sublime
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.get('/', function (req, res) {
+ res.send('Hello World');
+});
+
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
+});
