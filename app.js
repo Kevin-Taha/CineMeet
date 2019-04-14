36517b0ca4fa4f4f6863e377fb07785184bd5cb8
yatharthrawat <rawat.yatharth@gmail.com>
2019-04-14 01:08:07 -0400

    Added Environment Variable and User Methods to Change Firebase Database.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // in sublime
-var express = require('express');
+var express = require("express");
+var firebase = require("firebase");
+const User = require("./User.js");
 var port = process.env.PORT || 3000;
-
 var app = express();
 
-app.get('/', function (req, res) {
- res.send('Hello World');
+const config = JSON.parse(process.env.firebaseConfig);
+firebase.initializeApp(config);
+
+app.get("/", function(req, res) {
+
+	res.send("Hello World");
 });
 
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
+app.listen(port, function() {
+  console.log(`Example app listening on port !`);
 });
