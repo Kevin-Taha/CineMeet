40948b717f5d3176c5789b86b353964f2f054545
kevintxy <ktaha@purdue.edu>
2019-04-20 15:37:54 -0400

    -

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/static/LoginScreen.html");
   let database = firebase.database();
   let dbRef = database.ref();
+
 });
 
 app.get("/Meet", function(req, res) {
