d74a7557495497fd5dc3753046253fb84de24b6d
jdwhite88 <white570@purdue.edu>
2019-04-23 11:54:22 -0400

    Return home from search & handle movie not found

--- functions/static/FindMovie.js
+++ functions/static/FindMovie.js
@@ -1,22 +1,47 @@
-async function getMovie(Movie) {
-    let omdbKey = "6280f60";
+async function displayMovie(Movie) {
     let movieData = await fetch(`http://www.omdbapi.com/?t=${Movie}&type=movie&apikey=46fcc81f`);
+
+    let movie = document.getElementById("movie-info");
     movieData = await movieData.json();
 
-    let movie = document.createElement("div");
-    movie.setAttribute("id", "movie");
+    if (movieData.ok) {
+        // Movie found
+        /*
+            <div id="movie-info">
+                <img src=${movie poster url} alt="Poster for ${movie title}">
+                <p>${movie title} (${movie year}, Rated {movie age rating}, ${movie duration})</p>
+            </div>
+        */
+        let poster = document.createElement("img");
+        poster.setAttribute("src", movieData["Poster"]);
+        poster.setAttribute("alt", `Poster for ${movieData["Title"]}`);
+        movie.appendChild(poster);
 
-    let poster = document.createElement("img");
-    poster.setAttribute("src", movieData["Poster"]);
-    poster.setAttribute("alt", `Poster for ${movieData["Title"]}`);
-    movie.appendChild(poster);
-    let caption = document.createElement("p");
-    let captionText = document.createTextNode(`${movieData["Title"]} (${movieData["Year"]}, Rated ${movieData["Rated"]}, ${movieData["Runtime"]})`);
-    caption.appendChild(captionText);
-    movie.appendChild(caption);
-    movie.appendChild(document.createElement("br"));
-    
-    document.getElementById("movie-info").appendChild(movie);
+        let caption = document.createElement("p");
+        let captionText = document.createTextNode(`${movieData["Title"]} (${movieData["Year"]}, Rated ${movieData["Rated"]}, ${movieData["Runtime"]})`);
+        caption.appendChild(captionText);
+        movie.appendChild(caption);
+    }
+    else {
+        // Movie not found
+        /*
+            <div id="movie-info">
+                <p>"${movie title}" not found!</p>
+            </div>
+        */
+        const urlParams = new URLSearchParams(window.location.search);
+        const movieName = urlParams.get("search");
 
-    console.log(response);
+        console.log(movie);
+
+       let result = document.createElement("p");
+       let resultText = document.createTextNode(`\"${movieName}\" not found!`);
+       result.appendChild(resultText);
+       movie.appendChild(result);
+    }
+}
+
+function searchAgain() {
+    // TODO: Redirect to specific user
+    window.location.replace('/Home');
 }
