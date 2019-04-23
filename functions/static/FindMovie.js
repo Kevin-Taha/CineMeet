a002928798e570f8f4c098bc95ef53b793e56767
jdwhite88 <white570@purdue.edu>
2019-04-23 14:03:46 -0400

    Show more movie results

--- functions/static/FindMovie.js
+++ functions/static/FindMovie.js
@@ -1,10 +1,18 @@
-async function displayMovie(Movie) {
-    let movieData = await fetch(`http://www.omdbapi.com/?t=${Movie}&type=movie&apikey=46fcc81f`);
+var page = 1;
+
+async function displayMovie(MovieName, Year) {
+    // Get single movie result from OMDB API
+    let omdbKey = "46fcc81f";
+    let omdbURL = `http://www.omdbapi.com/?t=${MovieName}&type=movie`;
+    if (Year !== null) {
+        omdbURL += `&y=${Year}`;
+    }
+    omdbURL += `&apikey=${omdbKey}`;
+    let movieData = await fetch(omdbURL);
 
     let movie = document.getElementById("movie-info");
-    movieData = await movieData.json();
-
     if (movieData.ok) {
+        movieData = await movieData.json();
         // Movie found
         /*
             <div id="movie-info">
@@ -23,25 +31,69 @@ async function displayMovie(Movie) {
         movie.appendChild(caption);
     }
     else {
+        movieData = await movieData.json();
         // Movie not found
         /*
-            <div id="movie-info">
+            <div id="movie">
                 <p>"${movie title}" not found!</p>
             </div>
         */
-        const urlParams = new URLSearchParams(window.location.search);
-        const movieName = urlParams.get("search");
-
-        console.log(movie);
-
        let result = document.createElement("p");
-       let resultText = document.createTextNode(`\"${movieName}\" not found!`);
+       let resultText = document.createTextNode(`\"${MovieName}\" not found!`);
        result.appendChild(resultText);
        movie.appendChild(result);
     }
 }
 
+async function showMoreMovies() {
+    let movieList = document.getElementById("movie-info-list");
+    // Bring up the next 10 search results
+    let urlParams = new URLSearchParams(window.location.search);
+    let movieName;
+    if (urlParams.has('search')) {
+        movieName = urlParams.get('search');
+    }
+    else {
+        alert(`Invalid Input, missing movie name`);
+    }
+    
+    // TODO: Don't keep sending queries if no more results to show
+
+    let omdbKey = "46fcc81f";
+    let omdbURL = `http://www.omdbapi.com/?s=${movieName}&type=movie&page=${page}`;
+    omdbURL += `&apikey=${omdbKey}`;
+    let movies = await fetch(omdbURL);
+    if (movies.ok) {
+        movies = await movies.json();
+        // Array of movies with title, year, type, poster, imdbID
+        movies = movies["Search"]; // Array of movies
+        // Add search results to list
+        /*
+            <ul id="movie-info-list">
+                ...
+                <li>${movie title} (${movie year})</li>
+                ...
+            </ul>
+        */
+        for (let i = 0; movies !== undefined && i < movies.length; i++) {
+            
+            let movieData = movies[i];
+            console.log(movieData);
+            let elem = document.createElement("li");
+            elem.appendChild(document.createTextNode(`${movieData["Title"]} (${movieData["Year"]})`));
+            movieList.appendChild(elem);
+        }
+    }
+    else {
+        // Do not add to list
+    }
+
+    page++;
+}
+
 function searchAgain() {
     // TODO: Redirect to specific user
     window.location.replace('/Home');
+
+    
 }
