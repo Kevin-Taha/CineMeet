0b6bff9ec20ffe4992006eae9ac6df6a10a095fe
jdwhite88 <white570@purdue.edu>
2019-04-23 10:54:20 -0400

    Make OMDB API GET request

--- functions/static/FindMovie.js
+++ functions/static/FindMovie.js
@@ -1,11 +1,6 @@
 async function getMovie(Movie) {
-    console.log(Movie);
-    // let response = await fetch(`http://www.omdbapi.com/?t=${Movie}&type=movie&apikey=${omdbKey}`, {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     },
-    // });
-    // response = await response.json();
-    // console.log(response);
+    let omdbKey = "6280f60";
+    let response = await fetch(`http://www.omdbapi.com/?t=${Movie}&type=movie&apikey=46fcc81f`);
+    response = await response.json();
+    console.log(response);
 }
